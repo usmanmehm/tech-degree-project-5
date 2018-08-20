fb4f391b97d614946f72ac8088236767950701b0
Usman Mehmood <usman.mehm@outlook.com>
2018-08-20 15:57:35 -0400

    Add search functionality and comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-let people = [];
+// This program reads from an API that generates random users and populates a
+// page with the details that are needed to display including name, age, DOB,
+// address, etc...
+
+let people = []; // this empty array is set to store the people's data
 $.ajax({
-  url: 'https://randomuser.me/api/?gender=male&results=12',
+  url: 'https://randomuser.me/api/?gender=male&results=12&nat=us,au,nz,gb',
   dataType: 'json',
   success: function(data) {
     people = data.results;
     let personHTML = '';
-    people.forEach((person, index) => {
+    people.forEach((person, index) => { //this function is adding the details to the HTML
       personHTML +=
       `<div class="card" id=${index}>
           <div class="card-img-container">
@@ -19,10 +23,10 @@ $.ajax({
       </div>`;
     });
     $('.gallery').html(personHTML);
-    console.log(data);
 }
 });
 
+// Adding the search bar!
 let searchHTML =
 `<form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -31,19 +35,21 @@ let searchHTML =
 
 $('.search-container').html(searchHTML);
 
+// personIndex is the variable we will use to target the appropriate person when doing
+// any action
 let personIndex = 0;
 
-const modalDiv = document.createElement('div');
+const modalDiv = document.createElement('div'); // adding the modal window as HTML
 modalDiv.className = 'modal-container';
 modalDiv.innerHTML = createModalHTML();
 const bodyElement = document.querySelector('body');
 bodyElement.appendChild(modalDiv);
-modalDiv.style.display = 'none';
+modalDiv.style.display = 'none'; // it will be hidden until clicked
 
-$('.gallery').on('click', (e) => {
+$('.gallery').on('click', (e) => { //when a person clicks the card, the modal window will appear
   let clickedClass = e.target.className;
   let card = '';
-  if(clickedClass === 'card') {
+  if(clickedClass === 'card') { // you can click anywhere on the card so need to specify and specify the card variable accordingly
     card = e.target;
   } else if (clickedClass === 'card-img-container' || clickedClass === 'card-info-container') {
     card = e.target.parentNode;
@@ -51,26 +57,39 @@ $('.gallery').on('click', (e) => {
     card = e.target.parentNode.parentNode;
   }
   let person = people[card.id];
-  changeModalHTML(person);
-  modalDiv.style.display = '';
-  personIndex = parseInt(card.id);
+  changeModalHTML(person); // modal window HTML is set to the person clicked
+  modalDiv.style.display = ''; // modal window appears
+  personIndex = parseInt(card.id); // this will be used in the next/prev button functions
 })
-
+// When the x is clicked
 $('.modal-close-btn').on('click', () => {
-  modalDiv.style.display = 'none';
+  modalDiv.style.display = 'none'; // when the x is clicked, the modal window is hidden
 })
-
+// When the next button is clicked
 document.querySelector('.modal-next').addEventListener('click', () => {
-  personIndex = personIndex < 11 ? personIndex + 1 : 0;
-  changeModalHTML(people[personIndex]);
+  personIndex = personIndex < 11 ? personIndex + 1 : 0; // if we get to the end of the array, we start at the beginning again
+  changeModalHTML(people[personIndex]); // the modal HTML is set to the next person
 })
-
+// When the previous button is clicked
 document.querySelector('.modal-prev').addEventListener('click', () => {
-  personIndex = personIndex > 0 ? personIndex - 1 : 11;
-  changeModalHTML(people[personIndex]);
+  personIndex = personIndex > 0 ? personIndex - 1 : 11; // if we get to the beginning of the array, we start at the end again
+  changeModalHTML(people[personIndex]); // the modal HTML is set to the prev person
 })
 
-function createModalHTML() {
+// This search function is implemented so that whenever the user enters anything, the results are updated
+$('.search-container').on('keyup', function(event) {
+  let searchVal = document.querySelector('.search-input').value;
+  people.forEach((person, index) => {
+    let personName = person.name.first + person.name.last;
+    if (personName.indexOf(searchVal) === -1) {
+      $(`#${index}`).hide(); // only those people's names that don't match the search field will be hidden
+    } else {
+      $(`#${index}`).show();
+    }
+  });
+});
+
+function createModalHTML() { // function to initialize the modal HTML
   let modalHTML =
   `<div class="modal-container">
       <div class="modal">
@@ -94,7 +113,7 @@ function createModalHTML() {
   return modalHTML;
 }
 
-function changeModalHTML(person) {
+function changeModalHTML(person) { // function to change the modal HTML depending on the person clicked/specified
   $('.modal-img').prop("src", person.picture.large);
   $('#modal-name').text(`${person.name.first} ${person.name.last}`);
   $('#modal-email').text(person.email);
